b0d9dffa1d3c7386d9984ad94542e95ff6c68314
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-07-04 18:11:43 +0300

    Added user seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'account_id' => 1,
+            'name' => 'Dovakhin',
+            'email' => 'orcun.otacioglu@acikgise.com',
+            'password' => bcrypt('CPGKhrs7V')
+        ]);
+    }
+}
