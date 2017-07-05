1899ecc11496e1686af7049ab71fc6cf32917319
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-07-05 17:58:37 +0300

    Modified Account create page

--- resources/views/dashboard/account/create.blade.php
+++ resources/views/dashboard/account/create.blade.php
@@ -3,71 +3,73 @@
 @section('title', 'Create New Account')
 
 @section('content')
-    <form action="{{ action('AccountController@store') }}" method="POST">
-        {{ csrf_field() }}
-        <div class="row">
-            <div class="col-md-6">
+    <div class="col-md-12">
+        <form action="{{ action('AccountController@store') }}" method="POST">
+            {{ csrf_field() }}
+            <div class="row">
+                <div class="col-md-6">
 
-                <div class="form-group">
-                    <label for="name">Account Name</label>
-                    <input type="text" name="name" id="name" class="form-control">
+                    <div class="form-group">
+                        <label for="name">Account Name</label>
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="address">Company Address</label>
+                        <textarea name="address" id="address" cols="30" rows="2" class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="city">Company City</label>
+                        <input type="text" name="city" id="city" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="postalCode">Company Postal Code</label>
+                        <input type="text" name="postalCode" id="postalCode" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="country">Company Country</label>
+                        <input type="text" name="country" id="country" class="form-control">
+                    </div>
+
                 </div>
+                <div class="col-md-6">
 
-                <div class="form-group">
-                    <label for="address">Company Address</label>
-                    <textarea name="address" id="address" cols="30" rows="2" class="form-control"></textarea>
+                    <div class="form-group">
+                        <label for="about">About Company</label>
+                        <textarea name="about" id="about" cols="30" rows="2" class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="website">Company Website</label>
+                        <input type="text" name="website" id="website" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="facebookPage">Company Facebook Page</label>
+                        <input type="text" name="facebookPage" id="facebookPage" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="twitterPage">Company Twitter Page</label>
+                        <input type="text" name="twitterPage" id="twitterPage" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="phone">Company Phone Number</label>
+                        <input type="text" name="phone" id="phone" class="form-control">
+                    </div>
+
                 </div>
-
-                <div class="form-group">
-                    <label for="city">Company City</label>
-                    <input type="text" name="city" id="city" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="postalCode">Company Postal Code</label>
-                    <input type="text" name="postalCode" id="postalCode" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="country">Company Country</label>
-                    <input type="text" name="country" id="country" class="form-control">
-                </div>
-
             </div>
-            <div class="col-md-6">
-
-                <div class="form-group">
-                    <label for="about">About Company</label>
-                    <textarea name="about" id="about" cols="30" rows="2" class="form-control"></textarea>
+            <div class="row">
+                <div class="col-md-4">
+                    <input type="submit" value="Create" class="btn btn-primary">
+                    <a href="{{ action('AccountController@index') }}">Cancel</a>
                 </div>
-
-                <div class="form-group">
-                    <label for="website">Company Website</label>
-                    <input type="text" name="website" id="website" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="facebookPage">Company Facebook Page</label>
-                    <input type="text" name="facebookPage" id="facebookPage" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="twitterPage">Company Twitter Page</label>
-                    <input type="text" name="twitterPage" id="twitterPage" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="phone">Company Phone Number</label>
-                    <input type="text" name="phone" id="phone" class="form-control">
-                </div>
-
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">
-                <input type="submit" value="Create" class="btn btn-primary">
-                <a href="{{ action('AccountController@index') }}">Cancel</a>
-            </div>
-        </div>
-    </form>
+        </form>
+    </div>
 @stop
