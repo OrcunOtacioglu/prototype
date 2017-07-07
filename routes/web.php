777e403460f6c94724c3b925be2bf20623cd166c
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-07-07 13:54:34 +0300

    Created routes for the resources

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,18 @@
 
 
 Route::get('/', function () {
-    return view('frontend.index');
+    $events = \App\Models\Event::all();
+    return view('frontend.index', compact('events'));
 });
 
 Auth::routes();
 
 Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function () {
     Route::get('/', 'DashboardController@index');
+    Route::get('/categories', 'Util\EventCategoryController@index');
     Route::resource('/account', 'AccountController');
-    Route::resource('/event', 'EventController');
+    Route::resource('/event', 'EventController', ['except' => 'show']);
+    Route::get('/event/{slug}', 'EventController@show');
+    Route::resource('/rate', 'TicketTypeController');
+    Route::resource('/ticket', 'TicketController');
 });
