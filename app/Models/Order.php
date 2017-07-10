36f280aeda43dd4d57d48c5bffa2f23c3002dde7
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-07-11 01:14:20 +0300

    Created Order and OrderItem resources functionality

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
@@ -20,5 +22,20 @@ class Order extends Model
     public function orderItems()
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    public static function createNew(Attendee $attendee)
+    {
+        $order = new Order();
+        $order->reference = str_random(6);
+        $order->attendee_id = $attendee->id;
+        $order->transaction_id = random_int(100000, 999999);
+        $order->status = 1;
+        $order->total = Cart::total();
+        $order->currency = 949;
+        $order->created_at = Carbon::now('Europe/Istanbul');
+        $order->updated_at = Carbon::now('Europe/Istanbul');
+        $order->save();
+        OrderItem::createItems($order);
     }
 }
