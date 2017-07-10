797d0630d13bb6f9818ec8a734203b5f187ef13e
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-07-10 17:20:47 +0300

    Added routes for new resources

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,15 @@ Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function () {
     Route::get('/categories', 'Util\EventCategoryController@index');
     Route::resource('/account', 'AccountController');
     Route::resource('/event', 'EventController', ['except' => 'show']);
-    Route::get('/event/{slug}', 'EventController@show');
     Route::resource('/rate', 'TicketTypeController');
     Route::resource('/ticket', 'TicketController');
+    Route::resource('/order', 'OrderController');
 });
+
+Route::get('/event/{slug}', 'EventController@show');
+Route::post('/cart', 'CartController@addItem');
+Route::get('/cart', 'CartController@show');
+Route::get('/remove/{id}', 'CartController@deleteItem');
+Route::get('/cart-destroy', 'CartController@destroyCart');
+
+Route::resource('/attendee', 'AttendeeController');
