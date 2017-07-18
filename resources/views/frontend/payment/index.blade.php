52cc7a6c245064e39c759df5203a77a7f2b5131c
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-07-18 17:21:46 +0300

    Modified the views

--- resources/views/frontend/payment/index.blade.php
+++ resources/views/frontend/payment/index.blade.php
@@ -1,1 +1,17 @@
-<div id="iyzipay-checkout-form" class="responsive"></div>
+@extends('frontend.layout')
+
+@section('title', 'Complete Your Order')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div id="iyzipay-checkout-form" class="responsive"></div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('footer.scripts')
+    {!! $paymentForm !!}
+@stop
