d87490b2809078617077276c0e7182902d93809a
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-07-19 17:22:51 +0300

    Created successful order event and listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        'App\Events\OrderSuccessful' => [
+            'App\Listeners\ReserveTickets',
+            'App\Listeners\SendOrderConfirmationMail',
         ],
     ];
 
