90423c13d68358e7ae6c2a15d183f4f1c8322246
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-08-14 11:26:31 +0300

    Added footer

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,4 +1,17 @@
 <footer style="bottom: 0; position: relative; z-index: 0; width: 100%; margin-top: 30px;">
     <div class="container">
+        <div class="col-md-4">
+            <ul>
+                <li>
+                    <a href="{{ action('Util\PageController@show', ['slug' => 'satis-sozlesmesi']) }}">Satış Sözleşmesi</a>
+                </li>
+                <li>
+                    <a href="{{ action('Util\PageController@show', ['slug' => 'gizlilik-ve-kisisel-verilerin-korunumu']) }}">Gizlilik ve Kişisel Veriler</a>
+                </li>
+                <li>
+                    <a href="{{ action('Util\PageController@show', ['slug' => 'kullanim-kosullari']) }}">Kullanım Şartları</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </footer>
