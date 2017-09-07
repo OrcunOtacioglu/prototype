d9687f04666d5b9a18c32f41af58c456292b87ae
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-09-07 15:41:45 +0300

    testing

--- resources/views/auth/forms/dashboard-login.blade.php
+++ resources/views/auth/forms/dashboard-login.blade.php
@@ -1,4 +1,4 @@
-<form method="POST" action="{{ route('dashboard.login') }}">
+<form method="POST" action="{{ route('login') }}">
     {{ csrf_field() }}
 
     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
