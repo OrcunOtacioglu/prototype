a9f04d25c13816e05ac23158bf992041ed349109
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-09-19 14:18:06 +0300

    Changes for OnlineFBB requirements

--- resources/views/frontend/event/show.blade.php
+++ resources/views/frontend/event/show.blade.php
@@ -17,37 +17,53 @@
         <div class="coverPhotoContainer" style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('/images/cover-images/{{$event->bg_cover_image}}');">
             <div class="coverPhotoImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.15) 70%, rgba(0, 0, 0, 0.8) 100%), url('/images/cover-images/{{$event->cover_image}}'), linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5));">
                 <div class="coverTitle">
-                    <h2 class="eventOrganizer">{{ $event->account->name }}</h2>
                     <h1 class="eventTitle">{{ $event->title }}</h1>
                     <div class="eventLocation">{{ $event->location }}</div>
-                    <div class="eventDate">{{ $event->start_date }}</div>
+                    <div class="eventDate">{{ \Carbon\Carbon::parse($event->start_date)->diffForHumans() }}</div>
                 </div>
             </div>
         </div>
     </div>
     <div class="container">
-        <div class="row" style="background: #fff;">
+        <div class="row" style="background: #fff; margin-bottom: 50px;">
             <div class="col-md-8">
 
                 <h3>Etkinlik Detayı</h3>
                 <p>{{ $event->description }}</p>
-                <hr>
-
-                <ticket-select></ticket-select>
 
             </div>
 
             <div class="col-md-4">
 
-                <h3>Organizatör</h3>
-                <p style="margin: 0;">
-                    <a href="{{ action('AccountController@organizer', ['id' => $event->account->id]) }}">{{ $event->account->name }}</a>
-                </p>
-                <small>{{ substr($event->account->about, 0, 160) }}...</small>
+                <div class="BuyButton">
+                    <div>
+                        <div class="BuyButton__container">
+                            <div class="BuyButton__price">
+                                <div class="BuyButton__price__label">
+                                    <span>Fiyat</span>
+                                </div>
+                                <div class="BuyButton__price__amount">
+                                    <div>
+                                        <span>{{ $event->price }} TL</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="BuyButton__tickets">
+                                <div class="BuyButton__details__container">
 
-                <hr>
+                                </div>
 
-                <shopping-cart></shopping-cart>
+                                <form action="{{ action('OrderController@store') }}" method="POST" style="width: 100%;">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" name="eventID" value="{{ $event->id }}">
+                                    <input class="BuyButton__tickets__button button" type="submit" value="Şimdi İzle">
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
         @include('frontend.ticketType.show')
