ef322a250f66c2069285252d3f3723a1ca71c39f
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-10-11 14:47:00 +0300

    Bug Fixes

--- app/Http/Controllers/Auth/Account/LoginController.php
+++ app/Http/Controllers/Auth/Account/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class LoginController extends Controller
 {
