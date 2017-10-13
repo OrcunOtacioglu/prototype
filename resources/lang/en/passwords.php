fa309cac2a0ca6804f61b17aa623c81e10fc0fd8
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-10-13 10:46:27 +0300

    Turkish translations

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Parolanız en az 6 karakterden oluşmalıdır.',
+    'reset' => 'Parolanız başarıyla yenilendi!',
+    'sent' => 'Parola güncelleme linkinizi email adresinize gönderdik!',
+    'token' => 'Bu parola resetleme tokeni geçersiz.',
+    'user' => "Bu email adresi ile kayıtlı bir kullanıcı bulunamadı.",
 
 ];
