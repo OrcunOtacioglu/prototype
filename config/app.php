5ee8aa56d8013fbacb616e5109a418e59f082da3
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-10-13 14:26:50 +0300

    Changes on OrderConfirmation mail

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'AçıkGişe'),
+    'name' => env('APP_NAME', 'Neredeysen Orada'),
 
     /*
     |--------------------------------------------------------------------------
