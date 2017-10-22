ed54d87d489aec847717fc01c6448d93092f126a
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-10-22 23:35:21 +0300

    Changed password reset email

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,58 +1,53 @@
-@component('mail::message')
-{{-- Greeting --}}
-@if (! empty($greeting))
-# {{ $greeting }}
-@else
-@if ($level == 'error')
-# Whoops!
-@else
-# Hello!
-@endif
-@endif
-
-{{-- Intro Lines --}}
-@foreach ($introLines as $line)
-{{ $line }}
-
-@endforeach
-
-{{-- Action Button --}}
-@isset($actionText)
-<?php
-    switch ($level) {
-        case 'success':
-            $color = 'green';
-            break;
-        case 'error':
-            $color = 'red';
-            break;
-        default:
-            $color = 'blue';
-    }
-?>
-@component('mail::button', ['url' => $actionUrl, 'color' => $color])
-{{ $actionText }}
-@endcomponent
-@endisset
-
-{{-- Outro Lines --}}
-@foreach ($outroLines as $line)
-{{ $line }}
-
-@endforeach
-
-{{-- Salutation --}}
-@if (! empty($salutation))
-{{ $salutation }}
-@else
-Regards,<br>{{ config('app.name') }}
-@endif
-
-{{-- Subcopy --}}
-@isset($actionText)
-@component('mail::subcopy')
-"{{ $actionText }}" butonuna tıklamakta sorun yaşıyorsanız, aşağıdaki URL'yi kopyalayarak
-tarayıcınıza yapıştırabilirsiniz: [{{ $actionUrl }}]({{ $actionUrl }})
-@endcomponent
-@endisset
-@endcomponent
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="">
+</head>
+<body style="font-family: sans-serif; width: 100%">
+<table style="font-family: sans-serif; width: 620px">
+    <tbody>
+    <tr>
+        <td colspan="3"  style="border-bottom: 1px solid #353535">
+            <h2>Merhaba!</h2>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3">
+            <br>
+            <p>Bu maili parola sıfırlama talebinde bulunduğunuz için alıyorsunuz.</p>
+            <br>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3"  style="text-align: center;">
+            <a href="{{ $actionUrl }}" class="btn btn-primary" style="background-color: #fcb131; border: none; padding: 6px 25px; color: white;">Parolamı Sıfırla</a>
+        </td>
+    </tr>
+    </tbody>
+</table>
+<table style="font-family: sans-serif; width: 620px">
+    <tbody>
+    <tr>
+        <td colspan="3">
+            <br>
+            <p>Bu yönde bir talebiniz olmadıysa, herhangi bir işlem yapmanıza gerek yoktur!</p>
+            <p style="font-size: 12px;">"{{ $actionText }}" butonuna tıklamakta sorun yaşıyorsanız, aşağıdaki URL'yi kopyalayarak
+                tarayıcınıza yapıştırabilirsiniz: [{{ $actionUrl }}]({{ $actionUrl }})</p>
+            <br>
+        </td>
+    </tr>
+    </tbody>
+</table>
+<table style="background: #f1f1f1; width: 620px" border="0">
+    <tbody>
+    <tr >
+        <td style="text-align: left"><a href="http://neredeysenorada.com/" target="_blank"><img src="http://neredeysenorada.com/frontend/img/logo.png"></a></td>
+        <td>&nbsp;</td>
+        <td style="text-align: right; font-size: 12px">© 2017 Neredeysen Orada. Tüm hakları saklıdır.&nbsp;&nbsp;&nbsp;&nbsp;</td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
