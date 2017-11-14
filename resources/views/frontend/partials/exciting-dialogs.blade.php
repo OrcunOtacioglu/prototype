e53d1ac13fc5c4112e0c495b9304128e70ea3925
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-11-14 09:51:17 +0300

    Mobile view changes on new widget

--- resources/views/frontend/partials/exciting-dialogs.blade.php
+++ resources/views/frontend/partials/exciting-dialogs.blade.php
@@ -21,61 +21,129 @@
                     <div class="content">
                         <section id="muzik">
                             <div class="row list_tours_tabs">
-                                <div class="col-md-3 col-sm-4 col-xs-12">
+                                <div class="col-md-3 col-sm-4 col-xs-7">
                                     <div class="ribbon_4"><span>MÜZİK</span></div>
                                     <img src="assets/frontend/img/sarper.jpg" class="img-responsive img-shadow">
                                 </div>
-                                <div class="col-md-9 col-sm-8 col-xs-12 mt-75 mt-20">
+                                <div class="col-md-9 col-sm-8 col-xs-5 mt-20 mt-75">
                                     <h2 class="h2-sub">Sarper SEMİZ'le Heye<span class="color-red-default">Canlı</span> Sohbetler</h2>
+                                </div>
+                                <div class="col-md-9 col-sm-8 col-xs-12 mt-20">
                                     <span>Her hafta bir ünlünün konuk olacağı, canlı müzik ve renkli sohbetleriyle eğlenceyi ekranlarınıza taşıyacak olan Sarper Semiz’in “Heyecanlı Sohbetler” programı 8 Ocak’tan itibaren her pazartesi 21:00 - 22:30 saatleri arasında canlı olarak sadece neredeysenorada.com’da.</span>
                                 </div>
                             </div>
                         </section>
                         <section id="sinema">
                             <div class="row list_tours_tabs">
-                                <div class="col-md-3 col-sm-4 col-xs-12">
+                                <div class="col-md-3 col-sm-4 col-xs-7">
                                     <img src="assets/frontend/img/sinema_icon.png" class="img-responsive">
                                 </div>
-                                <div class="col-md-9 col-sm-8 col-xs-12 mt-75">
+                                <div class="col-md-9 col-sm-8 col-xs-5 mt-20 mt-75">
                                     <h2 class="h2-sub">Sinema</h2>
+                                </div>
+                                <div class="col-md-9 col-sm-8 col-xs-12 mt-20">
                                     <span>Ünlü oyuncuların sinema ve oyunculuk üzerine her hafta farklı bir konuğun katılımıyla gerçekleştirilecekleri sohbet programı yakında sadece neredeysenorada.com'da.</span>
                                 </div>
                             </div>
                         </section>
                         <section id="sahne">
                             <div class="row list_tours_tabs">
-                                <div class="col-md-3 col-sm-4 col-xs-12">
+                                <div class="col-md-3 col-sm-4 col-xs-7">
                                     <img src="assets/frontend/img/sahne_icon.png" class="img-responsive">
                                 </div>
-                                <div class="col-md-9 col-sm-8 col-xs-12 mt-75">
+                                <div class="col-md-9 col-sm-8 col-xs-5 mt-20 mt-75">
                                     <h2 class="h2-sub">Sahne</h2>
+                                </div>
+                                <div class="col-md-9 col-sm-8 col-xs-12 mt-20">
                                     <span>Tiyatronun usta isimlerinin yer alacağı, işin mutfağından sahneye kadar olan süreçte yaşananlarla ilgili konuklarla yapılacak söyleşilere ev sahipliği yapacak program, çok yakında neredeysenorada.com’da.</span>
                                 </div>
                             </div>
                         </section>
                         <section id="edebiyat">
                             <div class="row list_tours_tabs">
-                                <div class="col-md-3 col-sm-4 col-xs-12">
+                                <div class="col-md-3 col-sm-4 col-xs-7">
                                     <img src="assets/frontend/img/edebiyat_icon.png" class="img-responsive">
                                 </div>
-                                <div class="col-md-9 col-sm-8 col-xs-12 mt-75">
+                                <div class="col-md-9 col-sm-8 col-xs-5 mt-20 mt-75">
                                     <h2 class="h2-sub">Edebiyat</h2>
+                                </div>
+                                <div class="col-md-9 col-sm-8 col-xs-12 mt-20">
                                     <span>Sevilen yazarların, şairlerin ve bu işe gönül vermiş değerli isimlerin gerçekleştireceği derin sohbetler her hafta neredeysen orada canlı yayınlarıyla sizlerle buluşacak.</span>
                                 </div>
                             </div>
                         </section>
                         <section id="aktuel">
                             <div class="row list_tours_tabs">
-                                <div class="col-md-3 col-sm-4 col-xs-12">
+                                <div class="col-md-3 col-sm-4 col-xs-7">
                                     <img src="assets/frontend/img/aktuel_icon.png" class="img-responsive">
                                 </div>
-                                <div class="col-md-9 col-sm-8 col-xs-12 mt-75">
+                                <div class="col-md-9 col-sm-8 col-xs-5 mt-20 mt-75">
                                     <h2 class="h2-sub">Aktüel</h2>
+                                </div>
+                                <div class="col-md-9 col-sm-8 col-xs-12 mt-20">
                                     <span>Her hafta özel isimlerle gündeme dair en güncel ve merakla izleyeceğiniz farklı konuların ele alınacağı, konuşulmayanların konuşulacağı programı çok yakında buradan izleyebilirsiniz.</span>
                                 </div>
                             </div>
                         </section>
                     </div>
+                    {{--<div class="content">--}}
+                        {{--<section id="muzik">--}}
+                            {{--<div class="row list_tours_tabs">--}}
+                                {{--<div class="col-md-3 col-sm-4 col-xs-12">--}}
+                                    {{--<div class="ribbon_4"><span>MÜZİK</span></div>--}}
+                                    {{--<img src="assets/frontend/img/sarper.jpg" class="img-responsive img-shadow">--}}
+                                {{--</div>--}}
+                                {{--<div class="col-md-9 col-sm-8 col-xs-12 mt-75 mt-20">--}}
+                                    {{--<h2 class="h2-sub">Sarper SEMİZ'le Heye<span class="color-red-default">Canlı</span> Sohbetler</h2>--}}
+                                    {{--<span>Her hafta bir ünlünün konuk olacağı, canlı müzik ve renkli sohbetleriyle eğlenceyi ekranlarınıza taşıyacak olan Sarper Semiz’in “Heyecanlı Sohbetler” programı 8 Ocak’tan itibaren her pazartesi 21:00 - 22:30 saatleri arasında canlı olarak sadece neredeysenorada.com’da.</span>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</section>--}}
+                        {{--<section id="sinema">--}}
+                            {{--<div class="row list_tours_tabs">--}}
+                                {{--<div class="col-md-3 col-sm-4 col-xs-12">--}}
+                                    {{--<img src="assets/frontend/img/sinema_icon.png" class="img-responsive">--}}
+                                {{--</div>--}}
+                                {{--<div class="col-md-9 col-sm-8 col-xs-12 mt-75">--}}
+                                    {{--<h2 class="h2-sub">Sinema</h2>--}}
+                                    {{--<span>Ünlü oyuncuların sinema ve oyunculuk üzerine her hafta farklı bir konuğun katılımıyla gerçekleştirilecekleri sohbet programı yakında sadece neredeysenorada.com'da.</span>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</section>--}}
+                        {{--<section id="sahne">--}}
+                            {{--<div class="row list_tours_tabs">--}}
+                                {{--<div class="col-md-3 col-sm-4 col-xs-12">--}}
+                                    {{--<img src="assets/frontend/img/sahne_icon.png" class="img-responsive">--}}
+                                {{--</div>--}}
+                                {{--<div class="col-md-9 col-sm-8 col-xs-12 mt-75">--}}
+                                    {{--<h2 class="h2-sub">Sahne</h2>--}}
+                                    {{--<span>Tiyatronun usta isimlerinin yer alacağı, işin mutfağından sahneye kadar olan süreçte yaşananlarla ilgili konuklarla yapılacak söyleşilere ev sahipliği yapacak program, çok yakında neredeysenorada.com’da.</span>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</section>--}}
+                        {{--<section id="edebiyat">--}}
+                            {{--<div class="row list_tours_tabs">--}}
+                                {{--<div class="col-md-3 col-sm-4 col-xs-12">--}}
+                                    {{--<img src="assets/frontend/img/edebiyat_icon.png" class="img-responsive">--}}
+                                {{--</div>--}}
+                                {{--<div class="col-md-9 col-sm-8 col-xs-12 mt-75">--}}
+                                    {{--<h2 class="h2-sub">Edebiyat</h2>--}}
+                                    {{--<span>Sevilen yazarların, şairlerin ve bu işe gönül vermiş değerli isimlerin gerçekleştireceği derin sohbetler her hafta neredeysen orada canlı yayınlarıyla sizlerle buluşacak.</span>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</section>--}}
+                        {{--<section id="aktuel">--}}
+                            {{--<div class="row list_tours_tabs">--}}
+                                {{--<div class="col-md-3 col-sm-4 col-xs-12">--}}
+                                    {{--<img src="assets/frontend/img/aktuel_icon.png" class="img-responsive">--}}
+                                {{--</div>--}}
+                                {{--<div class="col-md-9 col-sm-8 col-xs-12 mt-75">--}}
+                                    {{--<h2 class="h2-sub">Aktüel</h2>--}}
+                                    {{--<span>Her hafta özel isimlerle gündeme dair en güncel ve merakla izleyeceğiniz farklı konuların ele alınacağı, konuşulmayanların konuşulacağı programı çok yakında buradan izleyebilirsiniz.</span>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</section>--}}
+                    {{--</div>--}}
                 </div>
             </div>
         </div>
